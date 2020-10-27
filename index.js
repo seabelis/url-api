61505f12473fdb5021a16b0f8cda588362ac9837
seabelis <seabelis74@gmail.com>
2020-10-27 15:43:25 +0100

    add cors

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const app = express()
+
+const corsMiddleware = require('cors')
+app.use(corsMiddleware)
+
 const port = process.env.PORT || 4000
+
 // const db = require("./db");
 // const Page = require("./page/model")
+
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
+
 const pageRouter = require("./page/router")
-
-
-
-
-
-app
-.use(pageRouter)
+app.use(pageRouter)
 .listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
